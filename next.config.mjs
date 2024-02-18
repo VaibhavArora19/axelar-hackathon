13aec00fd6ad70f19a5ee10f7261c0ef8782c761
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-02-18 22:39:29 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/create",
-        destination: "https://cosmiccowboys.cloud",
-        permanent: false,
-      },
-    ];
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
