d7335c1eaa53dc1db08995496a6b24abde4f1fe6
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-02-18 20:38:42 +0530

    added post url

--- app/api/end/route.ts
+++ app/api/end/route.ts
@@ -25,6 +25,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmeTa65VGaYty9s5tiBdqdQ78qZhWiAvcF49zaH8CrCVtD"/>
           <meta property="fc:frame:input:text" content="Congratulations"/>
+          <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/home"/>
           <meta property="fc:frame:button:1" content="Mint tokens" />
           <meta property="fc:frame:button:1:action" content="Link"/>
           <meta property="fc:frame:button:1:target" content="${process.env.NEXT_PUBLIC_BASE_URL}/create"/>
