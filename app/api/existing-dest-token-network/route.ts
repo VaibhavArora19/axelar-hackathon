8da617ec42ac2998a4e2fbe949ce396612798dcb
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-02-21 15:44:09 +0530

    fixed typo

--- app/api/existing-dest-token-network/route.ts
+++ app/api/existing-dest-token-network/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
           <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPbie6bPDZJYLFzt9org9ZLZXXGmiSPUnNGNGXtqNhCTF"/>
           <meta property="fc:frame:button:1" content="Base" />
           <meta property="fc:frame:button:2" content="Fantom"/>
-          <meta property="fc:frame:button:3" content content="Polygon"/>
+          <meta property="fc:frame:button:3" content="Polygon"/>
           <meta property="fc:frame:button:4" content="Bsc"/>
           <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/recipient-address"/>
           </head>
